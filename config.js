2cbc586aa9ab4756b8d93777d5a63f4afdde621c
root <root@server1.aidforlowincome.com>
2017-04-03 17:20:57 -0400

    config file

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    webserver: {
+        port: 4912,
+        host: '0.0.0.0'
+    },
+    redis: {
+        port: 5992,
+        host: '127.0.0.1',
+    },
+    home: {
+        latitude: 39.732224,
+        longitude: -104.983018
+    },
+};
