5dde0936deb108c5264f564960812f99628a2fca
root <root@server1.aidforlowincome.com>
2017-04-05 17:06:13 -0400

    added support for location based on client ip rather then lat/lng

--- webserver.js
+++ webserver.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-var app = require('express')(),
+var geoip = require('geo-from-ip'),
+    app = require('express')(),
     notams = require('notams'),
     c = require('chalk'),
     _ = require('underscore'),
@@ -20,12 +21,50 @@ redisServerProcess.stderr.on('data', function(dat) {
     console.log('redis server error:', dat.toString());
 });
 redisServerProcess.on('close', function(code) {
-//    console.log('redis process exited with code', code);
+    //    console.log('redis process exited with code', code);
 });
 console.log('Spawned redis server on host', config.redis.host, 'port', config.redis.port, 'as pid', redisServerProcess.pid);
 var redisClient = redis.createClient(config.redis.port, config.redis.host);
 var geo = require('georedis').initialize(redisClient);
 console.log('GeoRedis Service ready for locations import..');
+
+
+var getLocationInfo = function(req, res) {
+    if (!_.contains(['icaos', 'notams'], req.params.nearbyType))
+        return res.end('Invalid Nearby Type');
+    var distance = 50;
+    var unit = 'mi';
+    if (_.contains(['m', 'km', 'mi', 'ft'], req.params.unit))
+        unit = req.params.unit;
+    if (req.params.distance > 0 && req.params.distance < 200)
+        distance = req.params.distance;
+    getNearbyIcaos(req.params.lat, req.params.lng, distance, unit, function(err, nearbyLocations) {
+        if (err) throw err;
+        if (req.params.nearbyType == 'icaos')
+            return res.json(nearbyLocations.icaos);
+        else if (req.params.nearbyType == 'notams') {
+            notams(nearbyLocations.icaos, {
+                format: 'ICAO'
+            }).then(function(results) {
+                results = results.filter(function(r) {
+                    return r.notams.length > 0;
+                });
+                if (req.params.brief == 'brief')
+                    return res.json(results.map(function(r) {
+                        return {
+                            icao: r.icao,
+                            notamQty: r.notams.length,
+                            distance: _.findWhere(nearbyLocations.distances, {
+                                icao: r.icao
+                            }).distance,
+                            unit: unit,
+                        };
+                    }));
+                else return res.json(results);
+            });
+        }
+    });
+};
 var getNearbyIcaos = function(lat, lng, distance, unit, _fn) {
     geo.nearby({
         latitude: lat,
@@ -63,44 +102,15 @@ geo.addLocations(icaoLocations, function(err, reply) {
     if (err) throw err;
     else console.log('added', reply, 'ICAO locations to geo database');
 
-console.log(c.yellow('Creating API Endpoints...'));
+    console.log(c.yellow('Creating API Endpoints...'));
 
-    app.get('/nearby/:nearbyType/:lat/:lng/:distance/:unit/:brief?', function(req, res) {
-        if (!_.contains(['icaos', 'notams'], req.params.nearbyType))
-            return res.end('Invalid Nearby Type');
-        var distance = 50;
-        var unit = 'mi';
-        if (_.contains(['m', 'km', 'mi', 'ft'], req.params.unit))
-            unit = req.params.unit;
-        if (req.params.distance > 0 && req.params.distance < 200)
-            distance = req.params.distance;
-        getNearbyIcaos(req.params.lat, req.params.lng, distance, unit, function(err, nearbyLocations) {
-            if (err) throw err;
-            if (req.params.nearbyType == 'icaos')
-                return res.json(nearbyLocations.icaos);
-            else if (req.params.nearbyType == 'notams') {
-                notams(nearbyLocations.icaos, {
-                    format: 'ICAO'
-                }).then(function(results) {
-                    results = results.filter(function(r) {
-                        return r.notams.length > 0;
-                    });
-                    if (req.params.brief == 'brief')
-                        return res.json(results.map(function(r) {
-                            return {
-                                icao: r.icao,
-                                notamQty: r.notams.length,
-                                distance: _.findWhere(nearbyLocations.distances, {
-                                    icao: r.icao
-                                }).distance,
-                                unit: unit,
-                            };
-                        }));
-                    else return res.json(results);
-                });
-            }
-        });
+    app.get('/nearby/:nearbyType/myIP/:distance/:unit/:brief?', function(req, res) {
+        var ipDat = geoip.allData(req.connection.remoteAddress);
+        req.params.lat = ipDat.location.latitude;
+        req.params.lng = ipDat.location.longitude;
+        getLocationInfo(req, res);
     });
+    app.get('/nearby/:nearbyType/:lat/:lng/:distance/:unit/:brief?', getLocationInfo);
 
     app.get('/icaoNotams/:icaos/:notamFormat?', function(req, res) {
         if (req.params.notamFormat == 'DOMESTIC' || req.params.notamFormat == 'ICAO')
